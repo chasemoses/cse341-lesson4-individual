69ce557409ae536c36964f394e3092f2eab0024b
chasemoses <43392487+chasemoses@users.noreply.github.com>
2022-05-12 10:30:08 -0600

    Messing with cors pt4 - changing origin to herokuapp url

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 require('dotenv').config();
 
 var corsOptions = {
-  origin: 'https://localhost:8080/contacts',
+  origin: 'https://cse-341-lesson4.herokuapp.com/api-docs',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 

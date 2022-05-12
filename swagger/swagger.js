132dcb579328b47e4cf51ad539b28e7b8fc5d5fd
chasemoses <43392487+chasemoses@users.noreply.github.com>
2022-05-12 10:56:39 -0600

    Messing with cors pt8 - Changed swagger.js doc host to reflect heroku app link.

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Description',
   },
-  host: 'localhost:8080/contacts/',
+  host: 'cse-341-lesson4.herokuapp.com/',
   schemes: ['http'],
   tags: [
       {

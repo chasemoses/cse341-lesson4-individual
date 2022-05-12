3411b68b0909254758877307c6e54d9945aaecb6
chasemoses <43392487+chasemoses@users.noreply.github.com>
2022-05-12 10:11:35 -0600

    Added cors

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongodb = require('./mongoDB/connect');
 const app = express();
 const port = process.env.PORT || 8080;
+const cors = require('cors')
 
 require('dotenv').config();
 
 app
+.use(cors())
 .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
